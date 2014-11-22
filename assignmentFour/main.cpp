80ea77d0f4e1e42b3133fb63544f53569bb1f320
waseefakhtar <waseef@outlook.com>
2014-11-22 13:08:00 +0500

    Initial Commit

---
+++ assignmentFour/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignmentFour
+//
+//  Created by Waseef Akhtar on 11/22/14.
+//  Copyright (c) 2014 Waseef Akhtar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
